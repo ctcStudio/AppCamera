493b4e0aa4d85557ecf98db38649a4c91e923e06
HungHN <hunghn2612@gmail.com>
2017-01-04 21:24:56 +0700

    - home fragment 5%

---
+++ app/src/main/java/com/hiepkhach9x/appcamera/HomeFragment.java
@@ -0,0 +1,12 @@
+package com.hiepkhach9x.appcamera;
+
+/**
+ * Created by hungh on 1/4/2017.
+ */
+
+public class HomeFragment extends BaseFragment{
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_home;
+    }
+}
